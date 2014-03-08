6c4032387deb9b49d033bb6a5bcabd6893765baa
krll-tk <ndsdmfwg@gmail.com>
2014-03-08 19:49:33 +0400

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,40 @@
-var gulp = require('gulp');
+var  gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  coffee = require('gulp-coffee'),
+  less = require('gulp-less');
 
-gulp.task('test', function(){
-  console.log('test'); 
+gulp.task('connect', connect.server({
+  root: ['public'],
+  port: 1337,
+  livereload: true,
+  open: {
+    browser: 'google-chrome'
+  }
+}));
+
+gulp.task('coffee', function () {
+  gulp.src('./public/**/*.coffee')
+    .pipe(coffee())
+    .pipe(gulp.dest('./public/js'))
+    .pipe(connect.reload());
 });
+
+gulp.task('less', function () {
+  gulp.src('./public/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./public/css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+  gulp.src('./public/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./public/**/*.coffee'], ['coffee']);
+  gulp.watch(['./public/**/*.less'], ['less']);
+  gulp.watch(['./public/*.html'], ['html']);
+});
+
+gulp.task('default', ['connect', 'coffee', 'less', 'watch']);
